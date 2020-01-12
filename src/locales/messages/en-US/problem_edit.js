3f0b9c926e0feb3395c6652463aae6923fb7e198
Menci <huanghaorui301@gmail.com>
2020-01-12 11:07:23 +0000

    feat(problem): add en_US locale

---
+++ src/locales/messages/en-US/problem_edit.js
@@ -0,0 +1,65 @@
+module.exports = {
+  title_edit: "Edit Problem",
+  title_new: "New Problem",
+  header_edit: "Edit Problem {idString}",
+  header_new: "New Problem",
+  submit: "Submit",
+  submit_error: {
+    create: {
+      FAILED: "Failed to create new problem due to unknown error.",
+      PERMISSION_DENIED: "Permission denied."
+    },
+    update: {
+      FAILED: "Failed to update problem due to unknown error.",
+      NO_SUCH_PROBLEM: "No such problem.",
+      PERMISSION_DENIED: "Permission denied."
+    }
+  },
+  header_samples: "Samples",
+  content_editor: {
+    title: "Title",
+    preview_all: "Preview",
+    default: "Default language",
+    confirm_apply_template: "Confirm apply template",
+    confirm_delete: "Confirm delete language",
+    section_title: "Section Title",
+    preview: "Preview",
+    add_section: {
+      before_this_section: "Before this section",
+      after_this_section: "After this section"
+    },
+    section_content: "Section Content",
+    new_sample: "New Sample",
+    sample_input: "Input",
+    sample_output: "Output",
+    sample_explanation: "Explanation",
+    section_options: {
+      move_up: "Move Up",
+      move_down: "Move Down",
+      delete: "Delete",
+      confirm_delete: "Confirm Delete"
+    },
+    section_type: {
+      text: "Text",
+      sample: "Sample"
+    }
+  },
+  sample_editor: {
+    add_sample_when_empty: "Add Sample When Empty",
+    sample_id: "Sample ID",
+    warning: {
+      not_referenced: "Not referenced in language \"{language}\"",
+      multiple_references: "Referenced {referenceCount} times in language \"{language}\""
+    },
+    add_sample: {
+      before_this_sample: "Before this sample",
+      after_this_sample: "After this sample"
+    },
+    options: {
+      move_up: "Move Up",
+      move_down: "Move Down",
+      delete: "Delete",
+      confirm_delete: "Confirm Delete"
+    }
+  }
+};
