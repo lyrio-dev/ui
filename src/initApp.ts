8b6583de965a1e6adfb8366699994667eff3ea0c
Menci <huanghaorui301@gmail.com>
2020-03-21 17:03:02 +0000

    fix(init): fix appState.userPreference is undefined with invalid key

--- src/initApp.ts
+++ src/initApp.ts
@@ -9,7 +9,7 @@ export default async function initApp() {
     // Get current logged in user's meta
     const { requestError, response } = await AuthApi.getCurrentUserAndPreference();
     appState.loggedInUser = response.userMeta;
-    appState.userPreference = response.userPreference;
-    appState.serverPreference = response.serverPreference;
+    appState.userPreference = response.userPreference || {};
+    appState.serverPreference = response.serverPreference || {};
   }
 }
