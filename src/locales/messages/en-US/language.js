8a32c99a95e666b97072c466a093cdaf7e54c75b
Chanis <ChanisYP@outlook.com>
2020-03-26 23:16:33 +0800

    fixed again

--- src/locales/messages/en-US/language.js
+++ src/locales/messages/en-US/language.js
@@ -1,12 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-  zh_CN: "Chinese（Simplified）",
-  en_US: "English",
-  ja_JP: "Japanese"
-};
-=======
   zh_CN: "Chinese (Simplified)",
   en_US: "English",
   ja_JP: "Japanese"
 };
->>>>>>> 2c5a7b5d4b3cb445ca5657ae93177ea4d826be4c
