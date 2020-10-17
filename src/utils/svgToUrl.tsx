5ebb2be87efec7cc763644c9f837ddd2dc6c9ac4
Menci <huanghaorui301@gmail.com>
2020-10-17 13:38:48 +0800

    fix(utils): fix wrong svgToDataUrl() implementation
    ReactDOM.render() is asynchronous so the element may NOT be rendered after it's called. Use
    ReactDOMServer instead.

--- src/utils/svgToUrl.tsx
+++ src/utils/svgToUrl.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOMServer from "react-dom/server.browser";
 
 export default function svgToDataUrl(SvgComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>) {
   const div = document.createElement("div");
-  ReactDOM.render(<SvgComponent />, div);
+  div.innerHTML = ReactDOMServer.renderToString(<SvgComponent />);
 
   const svgString = new XMLSerializer().serializeToString(div.firstElementChild);
   return URL.createObjectURL(new Blob([svgString], { type: "image/svg+xml" }));
