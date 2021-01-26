a6e88e5ff6e4bdbd7637cb8cc9e323f162ddb466
xsun2001 <xcx14@outlook.com>
2021-01-27 01:30:34 +0800

    finish algorithm base

---
+++ src/pages/graph-editor/GraphStructure.test.ts
@@ -0,0 +1,48 @@
+import { Edge, Graph, GraphOption, WeightedEdge, WeightedEdgeDatum } from "./GraphStructure";
+import * as GraphData from "./test.json";
+
+test("Graph node init", () => {
+  let g = new Graph(GraphData.nc, undefined, GraphData.edges.map<Edge>(e => new Edge(e.source, e.target)));
+  g.getNodeList().forEach((n, i) => expect(n.id).toEqual(i));
+});
+
+test("AdjMat", () => {
+  let edges = [
+    [0, 1], [1, 2], [2, 3], [3, 4], [0, 2], [1, 4]
+  ].map<Edge>(e => new Edge(e[0], e[1]));
+  let expected = [
+    [0, 1, 1, 0, 0],
+    [1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 0],
+    [0, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0]
+  ];
+  let g = new Graph(5, undefined, edges);
+  expect(g.toAdjacencyMatrix()).toStrictEqual(expected);
+});
+
+test("AdjMat weighted", () => {
+  let edges = [
+    [0, 1, 10], [1, 2, 12], [2, 3, 32], [3, 4, 18], [0, 2, 92], [1, 4, 10]
+  ].map(e => new WeightedEdge(e[0], e[1], e[2]));
+  let expected = [
+    [0, 10, 92, 0, 0],
+    [10, 0, 12, 0, 10],
+    [92, 12, 0, 32, 0],
+    [0, 0, 32, 0, 18],
+    [0, 10, 0, 18, 0]
+  ];
+  let g = new Graph(5, undefined, edges, GraphOption.Weighted | GraphOption.NoMultipleEdges);
+  expect(g.toAdjacencyMatrix()).toStrictEqual(expected);
+});
+
+test("from AdjMat", () => {
+  let adjmat = [
+    [0, 10, 92, 0, 0],
+    [10, 0, 12, 0, 10],
+    [92, 12, 0, 32, 0],
+    [0, 0, 32, 0, 18],
+    [0, 10, 0, 18, 0]
+  ];
+  expect(adjmat).toStrictEqual(Graph.fromAdjacencyMatrix<undefined, WeightedEdgeDatum>(adjmat, false, true, undefined, v => ({ weight: v }))?.toAdjacencyMatrix());
+});
---
+++ src/pages/graph-editor/algorithms/DFS.test.ts
@@ -0,0 +1,23 @@
+import {DFSNodeDatum, DFS} from "./DFS";
+import {Graph, WeightedEdgeDatum} from "../GraphStructure";
+
+test("dfs", () => {
+    let mat = [
+        [0, 1, 1, 1, 0],
+        [6, 0, 2, 2, 0],
+        [0, 3, 0, 9, 0],
+        [3, 0, 7, 0, 8],
+        [0, 5, 0, 0, 0]
+    ];
+    let graph = Graph.fromAdjacencyMatrix(mat, true, true, undefined, v => new WeightedEdgeDatum(v));
+    let dfs = new DFS();
+    if (graph === null)
+        throw new Error();
+    else {
+        let steps = Array
+            .from(dfs.run(graph, 0))
+            .map(s => s.graph.getNodeList())
+            .map(nl => nl.map(n => n.datum?.dist));
+        console.table(steps);
+    }
+})
