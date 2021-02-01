8629e855b79a4dfca7c4fa1106776e18302d6ac1
xsun2001 <xcx14@outlook.com>
2021-02-01 12:21:39 +0800

    fix(algo): use example in the textbook. copy results of each step. rename hungarian algorithm.

--- src/pages/graph-editor/algorithms/BiregularMatch.test.ts
+++ src/pages/graph-editor/algorithms/BiregularMatch.test.ts
@@ -1,15 +1,14 @@
-import { BGM } from "./BiregularMatch";
+import { HungarianDFS } from "./BiregularMatch";
 import { BipartiteGraph, Edge, Graph } from "../GraphStructure";
 
-test("bgm", () => {
+test("HungarianDFS", () => {
   let edges = [
-    [0, 3], [0, 4], [0, 5], [1, 5], [2, 5]
+    [0, 6], [0, 8], [1, 9], [1, 11], [2, 7], [2, 9], [3, 10], [4, 10], [4, 11], [5, 11]
   ].map(([s, t]) => ({ source: s, target: t, datum: {} }));
-  let graph = new BipartiteGraph(3, 3, edges);
-  let bgm = new BGM();
-  let steps = Array
-    .from(bgm.run(graph))
-    .map(s => s.graph.edges())
-    .map(nl => nl.map(n => n.datum.tag));
-  console.table(steps);
+  let graph = new BipartiteGraph(6, 6, edges);
+  let res: number[][] = [];
+  for (let step of new HungarianDFS().run(graph)) {
+    res.push(step.graph.nodes().map(n => n.datum.match));
+  }
+  console.table(res);
 });
--- src/pages/graph-editor/algorithms/BiregularMatch.ts
+++ src/pages/graph-editor/algorithms/BiregularMatch.ts
@@ -1,9 +1,9 @@
 import { GraphAlgorithm, Step } from "../GraphAlgorithm";
 import { AdjacencyList, BipartiteGraph, Graph, Node, NodeEdgeList } from "../GraphStructure";
 
-class BGM extends GraphAlgorithm {
+class HungarianDFS extends GraphAlgorithm {
   constructor() {
-    super("BGM", "Biregular Graph Match");
+    super("HungarianDFS", "DFS version of Hungarian algorithm for Biregular Graph Match");
   }
 
   dfs(node: Node, edges: AdjacencyList): boolean {
@@ -34,77 +34,15 @@ class BGM extends GraphAlgorithm {
     }));
 
     for (let leftNode of left) {
+      edges.forEach(edge => edge.datum.matched = nodes[edge.source].datum.match === edge.target);
+      yield { graph: new NodeEdgeList(nodes, edges) };
+
       right.forEach(node => node.datum.visited = false);
       if (!this.dfs(leftNode, adjlist)) {
         leftNode.datum.tag = 2;
       }
-      edges.forEach(edge => edge.datum.matched = nodes[edge.source].datum.match === edge.target);
-      yield { graph: new NodeEdgeList(nodes, edges) };
     }
-
-    // let nc = graph.nodes(), ncL: number = 0;
-    // let nodes: Node[] = graph.getNodeList();
-    // let nL: number[] = [], nR: number[] = [];
-    // let side: BiregularSide[] = [];
-    // let tag: number[] = [];
-    // let visit: boolean[] = [], match: number[] = [];
-    //
-    // let edges = new AdjacencyEdges(nc);
-    //
-    // nodes.forEach((n, i) => {
-    //   side[i] = n.datum.side;
-    //   tag[i] = 0;
-    //   match[i] = -1;
-    //   if (side[i] == BiregularSide.Left)
-    //     ncL = nL.push(i);
-    //   else nR.push(i);
-    // });
-    // graph.getEdgeList().forEach((e, i) => {
-    //   // make sure it is a biregular graph.
-    //   if (side[e.source] == side[e.target])
-    //     throw new Error(`Edge ${e} is invalid.`);
-    //   if (side[e.source] == BiregularSide.Left) edges.addEdge(e);
-    //   else edges.addEdge(new Edge(e.target, e.source));
-    // });
-    //
-    // for (let i = 0; i < ncL; ++i) {
-    //   //console.log(nL[i]);
-    //   nR.forEach((pR) => {
-    //     visit[pR] = false;
-    //   });
-    //   if (!this.dfs(nL[i], edges, match, visit, tag))
-    //     tag[nL[i]] = 2;
-    //   let resEdges: Edge[] = [];
-    //   edges.dst.forEach((d, psrc) => {
-    //     d.forEach((pdst) => {
-    //       resEdges.push(new Edge(psrc, pdst, new BGMEdgeDatum(match[pdst] == psrc, false)));
-    //     });
-    //   });
-    //   let resGraph = new Graph(nc, (i: number) => new BGMNodeDatum(side[i], tag[i]), undefined, GraphOption.Simple, true);
-    //   if (resGraph) yield new Step(resGraph);
-    //   else throw new Error();
-    // }
   }
 }
 
-export { BGM };
-
-
-// int f[N],to[N*N],nt[N*N],a[N];
-// bool visit[N];
-// int dfs(int pos) {
-//     for(int i=f[pos]; i; i=nt[i])
-//     if(!visit[to[i]]) {
-//         visit[to[i]]=1;
-//         if(!a[to[i]]||dfs(a[to[i]])) {
-//             a[to[i]]=pos;
-//             return 1;
-//         }
-//     }
-//     return 0;
-// }
-//     for(int i=1; i<=n; ++i) {
-//         memset(vis,0,sizeof vis);
-//         ans+=dfs(i);
-//     }
-//     printf("%d\n",ans);
+export { HungarianDFS };
