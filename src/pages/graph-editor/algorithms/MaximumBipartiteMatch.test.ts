39227dcd8f9005c3593b77a39204b7c67cc29da2
shanjb0221 <2533296339@qq.com>
2021-02-05 19:16:16 +0800

    algo(Maximum Bipartite Match): add intermediate steps

--- src/pages/graph-editor/algorithms/MaximumBipartiteMatch.test.ts
+++ src/pages/graph-editor/algorithms/MaximumBipartiteMatch.test.ts
@@ -11,8 +11,20 @@ test("KuhnMunkres", () => {
   ].map(line => line.map(w => ({ weight: w })));
   let graph = new BipartiteMatrix(mat);
   let res: number[][] = [];
+  let resNode: any[][] = [];
+  let resEdge: any[][] = [];
   for (let step of new KuhnMunkres().run(graph)) {
     res.push(step.graph.nodes().map(n => n.datum.match));
+    resNode.push(step.graph.nodes().map(n => (
+      n.datum.l
+    )));
+    resEdge.push(step.graph.edges().map(e => (
+      (e.datum.valid ? "v" : " ") +
+      (e.datum.marked ? "m" : " ") +
+      (e.datum.matched ? "M" : " ")
+    )));
   }
   console.table(res);
+  console.table(resNode);
+  console.table(resEdge);
 });
