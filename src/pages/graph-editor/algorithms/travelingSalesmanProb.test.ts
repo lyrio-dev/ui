325d27a5d4f82d463f8622223b9c101366bdbda9
roufaen <1783586258@qq.com>
2021-02-05 19:24:40 +0800

    algo(Traveling Salesman Problem): bugs fixed

---
+++ src/pages/graph-editor/algorithms/travelingSalesmanProb.test.ts
@@ -0,0 +1,25 @@
+import { SalesmanPath } from "./travelingSalesmanProb";
+import { AdjacencyMatrix } from "../GraphStructure";
+
+test("SalesmanPath", () => {
+  let mat = [
+    [0, 10, 10, 9, 4],
+    [0, 0, 13, 4, 20],
+    [0, 0, 0, 11, 3],
+    [0, 0, 0, 0, 16],
+    [0, 0, 0, 0, 0]
+  ];
+  let graph = new AdjacencyMatrix(mat, true);
+  let output = [];
+  for (let step of new SalesmanPath().run(graph)) {
+    let tmp = [];
+    step.graph.edges().forEach(e => {
+      if (e.datum.chosen == 1) {
+        tmp.push([e.source + 1, e.target + 1]);
+      }
+    });
+    tmp.push(step.answer);
+    output.push(tmp);
+  }
+  console.table(output);
+});
