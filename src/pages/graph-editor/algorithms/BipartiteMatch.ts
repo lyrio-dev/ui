fea1944acfa3fae6a6b900c26d088ea6845b9b36
shanjb0221 <2533296339@qq.com>
2021-02-05 21:10:49 +0800

    refactor(algo Bipartite Match): reformat

--- src/pages/graph-editor/algorithms/BipartiteMatch.ts
+++ src/pages/graph-editor/algorithms/BipartiteMatch.ts
@@ -10,20 +10,22 @@ class HungarianDFS extends GraphAlgorithm {
   private nodes: Node[] = [];
   private adjlist: AdjacencyList;
 
-  * dfs(pos: number) {
-    let sn = this.nodes[pos], sd = sn.datum;
+  *dfs(pos: number) {
+    let sn = this.nodes[pos],
+      sd = sn.datum;
     for (let e of this.adjlist.adjacentEdges(sn.id)) {
-      let tn = this.nodes[e.target], td = tn.datum;
+      let tn = this.nodes[e.target],
+        td = tn.datum;
       if (!td.visit) {
         td.visit = true;
-        td.mark = sn.id, sd.mark = tn.id;
+        (td.mark = sn.id), (sd.mark = tn.id);
         if (td.match === -1 || (yield* this.dfs(td.match))) {
           if (td.match === -1) {
             this.edges.forEach(edge => (edge.datum.matched = this.nodes[edge.source].datum.match === edge.target));
             this.edges.forEach(edge => (edge.datum.marked = this.nodes[edge.source].datum.mark === edge.target));
             yield { graph: new NodeEdgeList(this.nodes, this.edges) };
           }
-          td.match = sn.id, sd.match = tn.id, td.tag = sd.tag = 1;
+          (td.match = sn.id), (sd.match = tn.id), (td.tag = sd.tag = 1);
           return true;
         }
         td.mark = sd.mark = -1;
@@ -39,19 +41,21 @@ class HungarianDFS extends GraphAlgorithm {
     let left = graph.leftSide,
       right = graph.rightSide;
     this.nodes.forEach(node => Object.assign(node.datum, { tag: 0, match: -1, mark: -1 }));
-    this.edges.map(edge => this.nodes[edge.source].datum.side === "left" ? edge : {
-        source: edge.target,
-        target: edge.source,
-        datum: edge.datum
-      }
+    this.edges.map(edge =>
+      this.nodes[edge.source].datum.side === "left"
+        ? edge
+        : {
+            source: edge.target,
+            target: edge.source,
+            datum: edge.datum
+          }
     );
     this.edges.forEach(e => Object.assign(e.datum, { marked: false, matched: false }));
 
     for (let leftnode of left) {
       right.forEach(rightnode => (this.nodes[rightnode.id].datum.visit = false));
       this.nodes.forEach(node => (node.datum.mark = -1));
-      if (!(yield* this.dfs(leftnode.id)))
-        this.nodes[leftnode.id].datum.tag = 2;
+      if (!(yield* this.dfs(leftnode.id))) this.nodes[leftnode.id].datum.tag = 2;
 
       this.edges.forEach(edge => (edge.datum.matched = this.nodes[edge.source].datum.match === edge.target));
       this.edges.forEach(edge => (edge.datum.marked = false));
