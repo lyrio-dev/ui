c8c0c2e85152e0d0a597288a40d577fafc9b6135
shanjb0221 <2533296339@qq.com>
2021-02-07 11:17:07 +0800

    algo(network flow): add large test case

--- src/pages/graph-editor/algorithms/networkflow/MaximumFlow.test.ts
+++ src/pages/graph-editor/algorithms/networkflow/MaximumFlow.test.ts
@@ -3,6 +3,7 @@ import { EdmondsKarp } from "./EdmondsKarp";
 import { Dinic } from "./Dinic";
 import { EdgeList } from "../../GraphStructure";
 
+import { graph, Tpos, Spos, limit } from "./LargeGraph.data";
 // let edges = [
 //   [0,1,3],
 //   [0,3,4],
@@ -16,42 +17,52 @@ import { EdgeList } from "../../GraphStructure";
 //   [4,5,6]
 // ].map(([s, t, f]) => ({ source: s, target: t, datum: {flow: f} }));
 // let graph = new EdgeList(6, edges);
-let edges = [
-  [0, 1, 1],
-  [0, 3, 1],
-  [1, 4, 1],
-  [2, 5, 1],
-  [3, 2, 1],
-  [3, 4, 1],
-  [4, 5, 1]
-].map(([s, t, f]) => ({ source: s, target: t, datum: { flow: f } }));
-let graph = new EdgeList(6, edges);
-let Spos = 0,
-  Tpos = 5;
+
+// let edges = [
+//   [0, 1, 1],
+//   [0, 3, 1],
+//   [1, 4, 1],
+//   [2, 5, 1],
+//   [3, 2, 1],
+//   [3, 4, 1],
+//   [4, 5, 1]
+// ].map(([s, t, f]) => ({ source: s, target: t, datum: { flow: f } }));
+// let graph = new EdgeList(6, edges);
+// let Spos = 0,
+//   Tpos = 5;
 
 test("FordFulkerson", () => {
   let resEdge: any[][] = [];
+  let count = 0;
   for (let step of new FordFulkerson().run(graph, Spos, Tpos)) {
-    resEdge.push(step.graph.edges().map(e => `[${e.datum.mark}](${e.datum.used}/${e.datum.flow})`));
+    ++count;
+    //resEdge.push(step.graph.edges().map(e => `[${e.datum.mark}](${e.datum.used}/${e.datum.flow})`));
   }
-  console.table(resEdge);
+  console.log(`algo FordFulkerson : step count = ${count}`);
+  //console.table(resEdge);
 });
 
 test("EdmondsKarp", () => {
   let resEdge: any[][] = [];
+  let count = 0;
   for (let step of new EdmondsKarp().run(graph, Spos, Tpos)) {
-    resEdge.push(step.graph.edges().map(e => `[${e.datum.mark}](${e.datum.used}/${e.datum.flow})`));
+    ++count;
+    //resEdge.push(step.graph.edges().map(e => `[${e.datum.mark}](${e.datum.used}/${e.datum.flow})`));
   }
-  console.table(resEdge);
+  console.log(`algo EdmondsKarp : step count = ${count}`);
+  //console.table(resEdge);
 });
 
 test("Dinic", () => {
   let resEdge: any[][] = [];
   let res: number[][] = [];
+  let count = 0;
   for (let step of new Dinic().run(graph, Spos, Tpos)) {
-    res.push(step.graph.nodes().map(n => n.datum.depth));
-    resEdge.push(step.graph.edges().map(e => `[${e.datum.valid}/${e.datum.mark}](${e.datum.used}/${e.datum.flow})`));
+    ++count;
+    //res.push(step.graph.nodes().map(n => n.datum.depth));
+    //resEdge.push(step.graph.edges().map(e => `[${e.datum.valid}/${e.datum.mark}](${e.datum.used}/${e.datum.flow})`));
   }
-  console.table(res);
-  console.table(resEdge);
+  console.log(`algo Dinic : step count = ${count}`);
+  //console.table(res);
+  //console.table(resEdge);
 });
