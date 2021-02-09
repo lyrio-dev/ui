8ab09b67d30a2780736b0dca409c29cefc09d3b4
roufaen <1783586258@qq.com>
2021-02-09 14:42:28 +0800

    algo(Prim): finish Prim

---
+++ src/pages/graph-editor/algorithms/Prim.test.ts
@@ -0,0 +1,18 @@
+import { Prim } from "./Prim";
+import { AdjacencyMatrix } from "../GraphStructure";
+
+test("CriticalPath", () => {
+  let mat = [
+    [0, 15, 0, 40, 25],
+    [15, 0, 30, 20, 30],
+    [0, 30, 0, 10, 15],
+    [40, 20, 10, 0, 10],
+    [25, 30, 15, 10, 0]
+  ];
+  let graph = new AdjacencyMatrix(mat, false);
+  let res = [];
+  for (let step of new Prim().run(graph)) {
+    res.push(step.graph.edges().map(e => e.source + ', ' + e.target + ', ' + e.datum.chosen));
+  }
+  console.table(res);
+});
