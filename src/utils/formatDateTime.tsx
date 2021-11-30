lyrio-dev/ui

commit 7d61889147aff093c12f47dc0de9c979b76af4ce

feat(submission): display a friendly submit time in submission item

src/utils/formatDateTime.tsx
@@ -31,3 +31,12 @@ export default function formatDateTime(
 
   return [withoutYear, withYear];
 }
+
+export function friendlyFormatDateTime(date: Date | string | number): [string | JSX.Element, string] {
+  if (!(date instanceof Date)) date = new Date(date);
+
+  const now = new Date();
+  const dateOnlyAndWithYear = date.getFullYear() === now.getFullYear();
+
+  return [formatDateTime(date, dateOnlyAndWithYear)[dateOnlyAndWithYear ? 1 : 0], formatDateTime(date)[1]];
+}

src/pages/submission/componments/SubmissionItem.tsx
@@ -5,7 +5,7 @@ import style from "./SubmissionItem.module.less";
 
 import { useLocalizer, Link } from "@/utils/hooks";
 import formatFileSize from "@/utils/formatFileSize";
-import formatDateTime from "@/utils/formatDateTime";
+import { friendlyFormatDateTime } from "@/utils/formatDateTime";
 import UserLink from "@/components/UserLink";
 import StatusText from "@/components/StatusText";
 import ScoreText from "@/components/ScoreText";
@@ -17,7 +17,7 @@ function parseSubmissionMeta(submission: ApiTypes.SubmissionMetaDto) {
   return {
     submission,
     submissionLink: `/s/${submission.id}`,
-    timeString: formatDateTime(submission.submitTime),
+    timeString: friendlyFormatDateTime(submission.submitTime),
     problemIdString: getProblemIdString(submission.problem),
     problemUrl: getProblemUrl(submission.problem)
   };

commit 747baf690e90864447bba75c70f61b511d3f7341

fix(submission): fix wrong date display caused by #18 (#20)

src/utils/formatDateTime.tsx
@@ -36,7 +36,7 @@ export function friendlyFormatDateTime(date: Date | string | number): [string | 
   if (!(date instanceof Date)) date = new Date(date);
 
   const now = new Date();
-  const dateOnlyAndWithYear = date.getFullYear() === now.getFullYear();
+  const dateOnlyAndWithYear = date.getFullYear() !== now.getFullYear();
 
   return [formatDateTime(date, dateOnlyAndWithYear)[dateOnlyAndWithYear ? 1 : 0], formatDateTime(date)[1]];
 }