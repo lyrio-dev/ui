a30f28fb455e93ece7acc275d3ac5d22b1eb6f50
Menci <huanghaorui301@gmail.com>
2022-08-24 17:38:39 +0800

    feat(initApp): use stale-while-revalidate cache for session

--- src/initApp.ts
+++ src/initApp.ts
@@ -1,6 +1,38 @@
 import { appState, initAppStateStore } from "@/appState";
 import { loadGoogleAnalytics, loadPlausible } from "@/misc/analytics";
 
+interface SessionSwrInfo {
+  version: number;
+  token: string;
+  sessionInfo: ApiTypes.GetSessionInfoResponseDto;
+  date: number;
+}
+
+const SESSION_SWR_INFO_KEY = "session-swr";
+const SESSION_SWR_INFO_VERSION = 1;
+const SESSION_SWR_INFO_VALID_FOR = 7 * 24 * 60 * 60 * 1000;
+
+function applySessionInfo(sessionInfo: ApiTypes.GetSessionInfoResponseDto) {
+  appState.currentUser = sessionInfo.userMeta;
+  appState.currentUserJoinedGroupsCount = sessionInfo.joinedGroupsCount;
+  appState.currentUserPrivileges = sessionInfo.userPrivileges || [];
+  appState.userPreference = sessionInfo.userPreference || {};
+  appState.serverPreference = sessionInfo.serverPreference;
+  appState.serverVersion = sessionInfo.serverVersion;
+}
+
+function saveSessionSwrInfo(sessionInfo: ApiTypes.GetSessionInfoResponseDto) {
+  localStorage.setItem(
+    SESSION_SWR_INFO_KEY,
+    JSON.stringify(<SessionSwrInfo>{
+      version: SESSION_SWR_INFO_VERSION,
+      token: appState.token,
+      sessionInfo,
+      date: Date.now()
+    })
+  );
+}
+
 // Wait for getSessionInfo JSONP API returns
 async function waitForSessionInitialization() {
   const sessionInfo =
@@ -13,17 +45,33 @@ async function waitForSessionInitialization() {
     }));
 
   delete window.sessionInfo;
+  applySessionInfo(sessionInfo);
+  saveSessionSwrInfo(sessionInfo);
+  console.log("session refreshed");
+}
 
-  appState.currentUser = sessionInfo.userMeta;
-  appState.currentUserJoinedGroupsCount = sessionInfo.joinedGroupsCount;
-  appState.currentUserPrivileges = sessionInfo.userPrivileges || [];
-  appState.userPreference = sessionInfo.userPreference || {};
-  appState.serverPreference = sessionInfo.serverPreference;
-  appState.serverVersion = sessionInfo.serverVersion;
+// SWR cache for getSessionInfo JSONP API
+function firstSessionInitialization() {
+  let sessionSwrInfo: SessionSwrInfo;
+  try {
+    sessionSwrInfo = JSON.parse(localStorage.getItem(SESSION_SWR_INFO_KEY));
+  } catch {}
+
+  if (
+    sessionSwrInfo?.version === SESSION_SWR_INFO_VERSION &&
+    Date.now() - sessionSwrInfo.date <= SESSION_SWR_INFO_VALID_FOR
+  ) {
+    console.log("session stale");
+    applySessionInfo(sessionSwrInfo.sessionInfo);
+  }
+
+  // revalidate (and apply new session info after)
+  waitForSessionInitialization();
 }
 
 export default async function initApp() {
-  await Promise.all([initAppStateStore(), waitForSessionInitialization()]);
+  await initAppStateStore();
+  firstSessionInitialization();
   loadGoogleAnalytics(appState.serverPreference.misc.googleAnalyticsId);
   loadPlausible(appState.serverPreference.misc.plausibleApiEndpoint);
 }
